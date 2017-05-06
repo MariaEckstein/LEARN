c1b727869018af53ed75fdeda48306f4c4004cba
Maria <maria.eckstein@berkeley.edu>
2017-05-05 22:52:26 -0700

    values as np array

--- main.py
+++ main.py
@@ -3,25 +3,27 @@ import numpy as np
 import math
 n_blue_lights = 9
 n_lights_lev0_tuple = 3
-n_colors = math.ceil(n_blue_lights ** (1/n_lights_lev0_tuple))
-# n_lights = n_blue_lights + n_blue_lights // 3 + n_blue_lights // 9
+n_trials = 100
+
+n_levels = math.ceil(n_blue_lights ** (1/n_lights_lev0_tuple))
 alpha = 0.5
+epsilon = 0.1
 
 
 class Environment(object):
     def __init__(self):
-        self.state = np.zeros((n_colors, n_blue_lights))
+        self.state = np.zeros((n_levels, n_blue_lights)).astype(np.int)
 
     def respond(self, action):
-        lightNumber, switchTo = action  # action in tuple-form: action = (lightNumber, switchTo)
-        self.state[0, lightNumber] = switchTo
-        self.do_events(lightNumber)
+        light_i, switch_to = action  # action in tuple-form: action = (light_i, switch_to)
+        self.state[0, light_i] = switch_to
+        self.do_events(light_i)
 
-    def do_events(self, lightNumber):
+    def do_events(self, light_i):
         n_levels = self.state.shape[0] - 1
-        for level in range(0, n_levels):  # "level" means color: blue, yellow, green, etc.
+        for level in range(n_levels):  # check for each level if rules fulfilled (blue, yellow, green, etc.)
             n_lights_tuple = n_lights_lev0_tuple * 3 ** level
-            first_in_tuple = lightNumber - (lightNumber % n_lights_tuple)
+            first_in_tuple = light_i - (light_i % n_lights_tuple)
             color_tuple = range(first_in_tuple, first_in_tuple + n_lights_tuple)
             if all(self.state[level, color_tuple]):  # if all lights in a tuple of this level are on
                 self.state[level + 1, color_tuple] = True  # turn next-level light on; take multiple columns
@@ -30,30 +32,36 @@ class Environment(object):
 
 class Agent(object):
     def __init__(self):
-        default = 0
-        self.v = defaultdict(lambda: default)  # v stores values for action tuples: v[(1,False)] = value of switching light 1 off
-        # defaultdict throws no error when a missing item is requested, instead it returns 0 (all v have 0 as default)
+        self.v = np.zeros([2, n_blue_lights])  # row0: values of turning off; row1: v of turning on; columns: lights
 
 
 class RewardAgent(Agent):
     def take_action(self, state):
-        possible_actions = [(i, False) if state[0, i] else (i, True) for i in range(n_blue_lights)]
-        return max(possible_actions, key=lambda a: self.v[a]) # simply return action with highest v
+        available_values = self.v[1-state[0], range(n_blue_lights)]
+        best_actions = np.argwhere(available_values == np.max(available_values)).flatten()
+        worse_actions = np.argwhere(available_values < np.max(available_values)).flatten()
+        if (np.random.rand() > epsilon) | (len(worse_actions) == 0):
+            light_i = np.random.choice(best_actions)
+        else:
+            light_i = np.random.choice(worse_actions)
+        switch_to = 1 - state[0, light_i]
+        return light_i, switch_to
 
     def update_values(self, old_state, action, new_state):
-        reward = sum(new_state[0,:]) - sum(old_state[0,:])
-        self.v[action] += alpha * (reward - self.v[action])
+        light_i, switch_to = action  # action in tuple-form: action = (light_i, switch_to)
+        reward = sum(new_state[0, :]) - sum(old_state[0, :])
+        self.v[switch_to, light_i] += alpha * (reward - self.v[switch_to, light_i])
 
 
 agent = RewardAgent()
 environment = Environment()
 
-for _ in range(30):
-    old_state = environment.state.copy() # ohne copy waeren old_state und new_state nur andere namen fuer environment.state
+for _ in range(n_trials):
+    old_state = environment.state.copy()  # ohne copy waeren old_state und new_state nur andere namen fuer environment.state
+    print(agent.v)
     action = agent.take_action(old_state)
     print(action)
     environment.respond(action)
     new_state = environment.state
     agent.update_values(old_state, action, new_state)
     print(new_state)
-    

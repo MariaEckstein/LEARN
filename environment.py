5b3518628675d7fe8b292e10d1272da9d199c926
Maria <maria.eckstein@berkeley.edu>
2017-06-23 18:51:22 -0700

    changed game such that events can always be created, not only when higher-level light is off

--- environment.py
+++ environment.py
@@ -17,8 +17,8 @@ class Environment(object):
             tuple_i = range(first_in_tuple, first_in_tuple + self.n_lights_tuple)
             tuple_complete = np.all(self.state[level, tuple_i])  # check if all lights in tuple_i are on
             next_level_light = first_in_tuple // self.n_lights_tuple
-            next_level_off = not self.state[level + 1, next_level_light]  # check if next-level light is off
-            if tuple_complete and next_level_off:
+            # next_level_off = not self.state[level + 1, next_level_light]  # check if next-level light is off
+            if tuple_complete:  # and next_level_off:
                 self.state[level, tuple_i] = 0  # turn off lower-level lights
                 self.state[level + 1, next_level_light] = 1  # turn on next-level lights
                 self.events[level + 1, next_level_light] = 1  # make a note of this event

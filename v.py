d1ed55ad69a21a782ac05df9f11056f9dccab40e
Maria <maria.eckstein@berkeley.edu>
2017-09-27 00:32:37 -0700

    use exponential decay for novelty decay

--- v.py
+++ v.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 class V(object):
-    def __init__(self, env):
+    def __init__(self, env, lambd):
+        self.lambd = lambd
         self.initial_value = 1 / env.n_lights_tuple / 2
         self.v = self.initial_value * np.ones([env.n_levels, env.n_lights])  # values of actions and options
         self.v[1:] = np.nan  # undefined for options
@@ -13,17 +14,13 @@ class V(object):
 
     def update(self, agent, option, goal_achieved, learning_signal='novelty', events=0):
         if learning_signal == 'novelty':
-            reward_signal = 1 / agent.n[option[0], option[1]]
+            reward_signal = np.exp(-self.lambd * agent.n[option[0], option[1]])
         elif learning_signal == 'reward':
             reward_signal = np.sum(events)
         else:
             print('Error! Learning signal must either be "novelty" or "reward"!')
         RPE = goal_achieved * reward_signal - self.v[option[0], option[1]]
         self.v[option[0], option[1]] += agent.alpha * RPE
-
-    # def reward_update(self, agent, option, goal_achieved):
-    #     reward = sum(new_state[0, :]) - sum(old_state[0, :])
-    #     self.v[action] += self.alpha * (reward - self.v[action])  # classic RL value update
 
     def get_option_values(self, state, option_stack, theta):
         inside_option = len(option_stack) > 0
